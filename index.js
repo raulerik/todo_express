8447bd8fc2b6df2d46112ffa8693d8884194581d
Raul-Erik Laur <raul.erik.laur@gmail.com>
2024-02-05 15:49:47 +0200

    Write data to file

--- index.js
+++ index.js
@@ -7,19 +7,50 @@ const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req,res) => {
-	// get data from file
-	fs.readFile('./tasks', 'utf8', (err, data) => {
+const readFile = (filename) => {
+return new Promise((resolve, reject) => {
+//get data from file
+	fs.readFile(filename, 'utf8', (err, data) => {
 		if (err) {
-			console.error(err);
-			return;
-		}
+		console.error(err);
+		return;
+	}
 // tasks list data from file
 	const tasks = data.split("\n")
+	resolve(tasks)
+		});
+	})
+}	
+
+app.get('/', (req,res) => {
+// tasks list data from file
+readFile('./tasks')
+.then(tasks => {
+	console.log(tasks)
 	res.render('index', {tasks:tasks})
 	});
 })
 
+//For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true}));
+
+app.post('/', (req, res) => {
+// tasks list data from file	
+	readFile('./tasks')
+	.then(tasks => {
+//add form send task to tasks array
+	tasks.push(req.body.task)
+	const data = tasks.join("\n")
+	fs.writeFile('./tasks', data, err => {
+		if (err) {
+		console.error(err);
+		return;
+	}
+// redirect to / to see result
+	res.redirect('/')
+		})	
+	})
+})
 app.listen(3001, () => {
 	console.log('Example app is started at http://localhost:3001')
 })
