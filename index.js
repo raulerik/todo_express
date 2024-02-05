cceb9e044381a5ebcaa0dbc881414c3114edea9b
Raul-Erik Laur <raul.erik.laur@gmail.com>
2024-02-05 15:44:03 +0200

    Read data from file

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const app = express()
+const fs = require('fs');
 
-// use ejs files to prepare templates for views
+// use ejs files to prepare templates for views 
 const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', (req,res) => {
+	// get data from file
+	fs.readFile('./tasks', 'utf8', (err, data) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+// tasks list data from file
+	const tasks = data.split("\n")
+	res.render('index', {tasks:tasks})
+	});
 })
 
 app.listen(3001, () => {
-    console.log('Example app is started at http://localhost:3001')
+	console.log('Example app is started at http://localhost:3001')
 })
